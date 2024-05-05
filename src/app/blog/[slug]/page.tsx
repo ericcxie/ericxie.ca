39db039ed36dcb262c1b49b6428fc7cb0e2ba431
Eric Xie <66566975+ericcxie@users.noreply.github.com>
2024-05-04 21:49:35 -0400

    Add first blog post

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -5,6 +5,10 @@ const Post = async ({ params }: { params: { slug: string } }) => {
 
   return (
     <section className="flex flex-col gap-5">
+      <h1 className="text-3xl font-bold">{postData.title}</h1>
+      <p className="text-text-light-body dark:text-text-dark-body">
+        {postData.date}
+      </p>
       <article
         className="post"
         dangerouslySetInnerHTML={{ __html: postData.contentHtml }}

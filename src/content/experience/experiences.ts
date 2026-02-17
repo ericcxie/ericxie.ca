303d03b1103529fd3398145cf83a828d1e9ddc20
Eric Xie <ex.ericxie@gmail.com>
2026-02-16 17:18:36 -0800

    edit

--- src/content/experience/experiences.ts
+++ src/content/experience/experiences.ts
@@ -1,15 +1,15 @@
 export const experienceItems = [
-  // {
-  //   company: "Warp",
-  //   position: "Software Engineer Intern",
-  //   location: "New York, NY",
-  //   date: "05/2026 - 08/2026",
-  //   logo: "/img/logos/warp.png",
-  //   color: "#000000",
-  //   link: "https://www.warp.dev/",
-  //   present: false,
-  //   incoming: true,
-  // },
+  {
+    company: "Warp",
+    position: "Software Engineer Intern",
+    location: "New York, NY",
+    date: "05/2026 - 08/2026",
+    logo: "/img/logos/warp.png",
+    color: "#000000",
+    link: "https://www.warp.dev/",
+    present: false,
+    incoming: true,
+  },
   {
     company: "Gem",
     position: "Software Engineer Intern",
